a8c60c25b3e184f0559928119072dcd7fddae9b6
Ethan Tarr <ebt0640@yahoo.com>
2017-01-21 11:36:36 -0800

    Updated the stuff
    Wave Smothness, curved map, bouncing waves, and stuff.

--- GGJ/Assets/PulseMove.cs
+++ GGJ/Assets/PulseMove.cs
@@ -7,6 +7,7 @@ public class PulseMove : MonoBehaviour {
 	public float speed = 5;
 	public float Amplitude = 1;
     public Color color = Color.white;
+	private bool forward = true;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +16,23 @@ public class PulseMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (new Vector3 (Time.deltaTime * speed, 0, 0));
+		if (transform.position.x < GameManager.boundary && forward) {
+			transform.Translate (new Vector3 (Time.deltaTime * speed, 0, 0));
+		} else if (forward) {
+			forward = false;
+			if (Amplitude < .5f) {
+				Destroy (this.gameObject);
+			}
+			Amplitude = Amplitude / 2;
+		} else if (!forward && transform.position.x > -GameManager.boundary) {
+			transform.Translate (new Vector3 (Time.deltaTime * -speed, 0, 0));
+		} else if (!forward) {
+			forward = true;
+			if (Amplitude < .5f) {
+				Destroy (this.gameObject);
+			}
+			Amplitude = Amplitude / 2;
+		}
 	}
 
 }
