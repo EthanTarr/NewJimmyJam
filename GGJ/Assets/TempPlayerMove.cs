c1d53427c1c92edff57e149fa372fc74e1d2a6ae
RetroSpecter <InsertNameHere4897@gmail.com>
2017-01-21 04:19:59 -0800

    animation

--- GGJ/Assets/TempPlayerMove.cs
+++ GGJ/Assets/TempPlayerMove.cs
@@ -5,28 +5,31 @@ using UnityEngine;
 public class TempPlayerMove : MonoBehaviour {
 
 	public float speed = 5;
+    public float jumpHeight = 3;
+
+    Rigidbody2D rigid;
 
 	// Use this for initialization
 	void Start () {
-		
+        rigid = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.W)) {
-			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2(0, speed * 15));
+			rigid.velocity =  new Vector2(0, jumpHeight);
 		} else if (Input.GetKey (KeyCode.A)) {
-			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2(-speed, 0));
+            rigid.velocity = new Vector2(-speed, 0);
 		} else if (Input.GetKey (KeyCode.S)) {
-			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2(0, -speed * 5));
+            rigid.AddForce (new Vector2(0, -speed * 5));
 		} else if (Input.GetKey (KeyCode.D)) {
-			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2(speed, 0));
+			rigid.AddForce (new Vector2(speed, 0));
 		} 
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.tag.Equals ("Floor") && other.relativeVelocity.magnitude > 8) {
-			GameObject.Find ("Main Camera").GetComponent<WaveGenerator> ().makeWave (transform.position.x, other.relativeVelocity.magnitude / 50f);
+			GameObject.Find ("Main Camera").GetComponent<WaveGenerator> ().makeWave (transform.position.x, other.relativeVelocity.magnitude / 100);
 		}
 	}
 }
--- GGJ/Assets/playertest.cs
+++ GGJ/Assets/playertest.cs
@@ -5,34 +5,64 @@ using UnityEngine;
 
 public class playertest : MonoBehaviour {
 
-/*    Rigidbody2D rigid;
+    Rigidbody2D rigid;
+    public LayerMask groundCheck;
     public Controls control;
+    [Space()]
+    public float speed = 5;
+    public float jumpHeight = 10;
+    Animator anim;
+    bool smashing;
 
     void Start() {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
-
-    void OnCollisionStay2D(Collision2D other) {
-        if(other.gameObject.tag != "Player")
-            rigid.velocity += other.gameObject.GetComponent<Rigidbody2D>().velocity / 2;
-    }
-
 
     void Update() {
-        if (Input.GetKey(control.right)) {
-            rigid.velocity = new Vector2(5, rigid.velocity.y);
-        } else if (Input.GetKey(control.left)) {
-            rigid.velocity = new Vector2(-5, rigid.velocity.y);
-        }
+        anim.SetFloat("velocity", 0);
+        bool touchingGround = checkGround();
+        if (!smashing)
+        {
+            if (Input.GetKey(control.right))
+            {
+                rigid.velocity = new Vector2(speed, rigid.velocity.y);
+                GetComponent<SpriteRenderer>().flipX = false;
+                anim.SetFloat("velocity", 1);
+            }
+            else if (Input.GetKey(control.left))
+            {
+                rigid.velocity = new Vector2(-speed, rigid.velocity.y);
+                GetComponent<SpriteRenderer>().flipX = true;
+                anim.SetFloat("velocity", 1);
+            }
 
-        if (Input.GetKeyDown(control.jump)) {
-            rigid.velocity = new Vector2(rigid.velocity.x, 10);
-        }
 
-        if (Input.GetKeyDown(control.down)) {
-            rigid.velocity = new Vector2(0, -10);
-        }
+            if (Input.GetKeyDown(control.jump) && touchingGround)
+            {
+                rigid.velocity = new Vector2(rigid.velocity.x, jumpHeight);
+            }
 
+            if (Input.GetKeyDown(control.down))
+            {
+                rigid.velocity = new Vector2(0, -10);
+                smashing = true;
+            }
+        }
+        anim.SetBool("smashing", smashing);
+    }
+
+
+    bool checkGround() {
+        RaycastHit2D hit = Physics2D.Raycast(GetComponent<Collider2D>().bounds.min, -transform.up, 0.5f, groundCheck);
+        Debug.DrawLine(GetComponent<Collider2D>().bounds.min, GetComponent<Collider2D>().bounds.min - transform.up * 0.5f);
+        anim.SetBool("airborne", hit == false);
+
+        return hit;
+    }
+
+    void recover() {
+        smashing = false;
     }
 
     float velocity;
@@ -41,9 +71,19 @@ public class playertest : MonoBehaviour {
     }
 
     void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.tag != "Player")
-            waveEffect.instance.startForce(Array.IndexOf(waveEffect.instance.segments, other.gameObject), Mathf.Abs(velocity) / 2);
-    }*/
+        if (other.gameObject.tag.Equals("Floor") && other.relativeVelocity.magnitude > 8) {
+            float strength = other.relativeVelocity.magnitude / 200f;
+            if (smashing) { strength *= 5f; }
+            GameObject.Find("Main Camera").GetComponent<WaveGenerator>().makeWave(transform.position.x, strength);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Floor")) {
+            //rigid.AddForce(transform.up * 100);
+        }
+    }
 }
 
 [Serializable]
