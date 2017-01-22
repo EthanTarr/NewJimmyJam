6f1a5f6a897afda477e0ece36a5d349f56127a10
Ethan Tarr <ebt0640@yahoo.com>
2017-01-22 07:42:26 -0800

    Title stuff
    yeah. It kind of got there.

---
+++ GGJ/Assets/TitleWaveMovers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleWaveMovers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, Mathf.Lerp(transform.position.y, 2.5f + Mathf.Sin(Time.time * transform.position.x), Time.deltaTime), 0);
+	}
+}
---
+++ GGJ/Assets/WaveForUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveForUI : MonoBehaviour {
+
+	public GameObject Square;
+	public float minFloorPlacement;
+	public float maxFloorPlacement;
+
+	// Use this for initialization
+	void Start () {
+		generateUISquares ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void generateUISquares() {
+		float width = Square.transform.localScale.x;
+		float spaceToFill = Mathf.Abs(minFloorPlacement) + Mathf.Abs(maxFloorPlacement) / width;
+		for (float i = -spaceToFill / 2; i < spaceToFill / 2f; i++) {
+			//Instantiate (Square, new Vector3 (.5f * i, -Mathf.Abs(Mathf.Pow(.08f *i,2)), 0), Quaternion.identity);
+			GameObject piece = Instantiate (Square, new Vector3 (width * i, 3, 0), Quaternion.identity);
+			piece.transform.localScale = new Vector3(piece.transform.localScale.x, piece.transform.localScale.y - (Mathf.Abs(i) * .05f), 0);
+			Color fade = piece.GetComponent<SpriteRenderer> ().color;
+			fade.a = fade.a - (Mathf.Abs (i) * .05f);
+			piece.GetComponent<SpriteRenderer> ().color = fade;
+		}
+	}
+}
