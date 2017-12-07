2252c826724b3615147ffe46628ba91bcd87ea9f
Ethan Tarr <ebt0640@yahoo.com>
2017-12-06 16:52:22 -0800

    Optomized Waves
    Waves are now calculated from the pulses instead of the squares.

--- GGJ/Assets/scripts/SquareBehavior.cs
+++ GGJ/Assets/scripts/SquareBehavior.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class SquareBehavior : MonoBehaviour {
 
 	public float TotalAmplitude;
+    private ArrayList Amplitudes;
     public float previousAmplitude;
-	public float Wavelength = 2f;
+	//public float Wavelength = 2f;
 	public float FloorOscillation = .02f;
     public float OscillationSpeed = 0.01f;
 	[HideInInspector] public float initialY = 0;
@@ -37,6 +38,10 @@ public class SquareBehavior : MonoBehaviour {
         radius = Mathf.Sqrt(Mathf.Pow(transform.localPosition.x, 2) + Mathf.Pow(transform.localPosition.y, 2));
 
         squareMaterial = transform.GetChild(0).GetComponent<Renderer>();
+
+        Amplitudes = new ArrayList();
+        //Debug.Log(this.gameObject.layer);
+
         //StartCoroutine(physicsCheck());
     }
 
@@ -48,6 +53,76 @@ public class SquareBehavior : MonoBehaviour {
 
     public float circleDampen = 1;
 
+    public void getPosition(float Amplitude, float speed, float Wavelength, Vector3 pulsePosition) {
+        if (TerrainGenerator.instance.shape == Shape.Sphere && circleLength == 0) {
+            circleLength = Mathf.Sqrt(Mathf.Pow(radius - Mathf.Cos(Wavelength / radius), 2) + Mathf.Pow(radius - Mathf.Sin(Wavelength / radius), 2));
+        }
+
+        standardY += FloorOscillation * (Mathf.Sin(Time.time * OscillationSpeed));
+        standardX += FloorOscillation * (Mathf.Sin(Time.time * OscillationSpeed));
+
+        float xPos = transform.position.x;
+        float xPulsePos = pulsePosition.x;
+
+        if (TerrainGenerator.instance.shape == Shape.Plane) {
+            Amplitudes.Add(Amplitude * (speed / 4) * Mathf.Sin(((Mathf.PI / Wavelength) * (xPos - xPulsePos))));
+        } else {
+            if (pulsePosition.y > CenterOfGravity.y) {
+                if ((initialY > 0 && standardX < pulsePosition.x) || (initialY <= 0 && initialX < 0)) {
+                    Amplitudes.Add((1 / (circleDampen * radius)) * Amplitude * Mathf.Sin((Mathf.PI / circleLength) * 
+                        (initialPositon - pulsePosition).magnitude));
+                }
+                else {
+                    Amplitudes.Add(-(1 / (circleDampen * radius)) * Amplitude * Mathf.Sin((Mathf.PI / circleLength) * 
+                        (initialPositon - pulsePosition).magnitude));
+                }
+            } else {
+                if ((initialY < 0 && standardX > pulsePosition.x) || (initialY >= 0 && initialX > 0)) {
+                    Amplitudes.Add((1 / (circleDampen * radius)) * Amplitude * Mathf.Sin((Mathf.PI / circleLength) * 
+                        (initialPositon - pulsePosition).magnitude));
+                } else {
+                    Amplitudes.Add(-(1 / (circleDampen * radius)) * Amplitude * Mathf.Sin((Mathf.PI / circleLength) * 
+                        (initialPositon - pulsePosition).magnitude));
+                }
+            }
+        }        
+    }
+
+    void setPosition() {
+        TotalAmplitude = 0;
+        if (Amplitudes.Count != 0) {
+            foreach (float value in Amplitudes)
+            {
+                TotalAmplitude += value;
+            }
+
+            Amplitudes = new ArrayList();
+        }
+
+        Vector3 vector = ((-((-transform.localPosition + new Vector3(0, 0, 0)).normalized)) * TotalAmplitude) / dampen;
+
+        if (TerrainGenerator.instance != null && TerrainGenerator.instance.shape == Shape.Sphere)
+        {
+            TotalAmplitude = Mathf.Clamp(TotalAmplitude, -maxCircleAmplitude, maxCircleAmplitude);
+            transform.localPosition = new Vector3(Mathf.Lerp(transform.localPosition.x, initialX + vector.x, Time.deltaTime),
+                Mathf.Lerp(transform.localPosition.y, initialY + vector.y, Time.deltaTime), 0);
+        }
+        else
+        {
+            //transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, standardY + vector.y, Time.deltaTime), transform.position.z);
+            TotalAmplitude = Mathf.Clamp(TotalAmplitude, -maxAmplitude, maxAmplitude);
+            transform.position = transform.right * transform.position.x + Vector3.up *
+                Mathf.Lerp(transform.position.y, standardY + vector.y, Time.deltaTime) + transform.forward * transform.position.z;
+        }
+
+        getVelocity();
+
+        if (firstBlock)
+        {
+            //GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, floorColor, Time.deltaTime);
+        }
+    }
+    /*
     void getPosition() {
 
         if (TerrainGenerator.instance.shape == Shape.Sphere && circleLength == 0) {
@@ -161,7 +236,7 @@ public class SquareBehavior : MonoBehaviour {
         if (firstBlock) {
             //GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, floorColor, Time.deltaTime);
         }
-    }
+    }*/
 
     [HideInInspector] public float velocity;
     void getVelocity() {
@@ -169,8 +244,8 @@ public class SquareBehavior : MonoBehaviour {
         lastPosition = transform.position;
     }
 
-    void Update(){
-            getPosition();
-            squareMaterial.material.SetColor("_Color", Color.Lerp(matColor, ampColor, (transform.localPosition.y - initialY)));
+    void LateUpdate(){
+        setPosition();
+        squareMaterial.material.SetColor("_Color", Color.Lerp(matColor, ampColor, (transform.localPosition.y - initialY)));
     }
 }
